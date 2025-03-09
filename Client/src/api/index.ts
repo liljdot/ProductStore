527e30b646276389fb35db8cec632b65a3c7c77b
liljdot <jehuoki@gmail.com>
2025-03-09 21:55:36 +0100

    update server baseurl for deployment

--- Client/src/api/index.ts
+++ Client/src/api/index.ts
@@ -1,5 +1,5 @@
 const host = window.location.host
-export const baseUrl = "http://" + host.slice(0, host.length - 4) + "3000"
+export const baseUrl = "https://productstore-3rxv.onrender.com"
 
 // export const baseUrl = "http://localhost:3000"
 // console.log(window.location)
