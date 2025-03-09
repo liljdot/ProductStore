0fac9b5eba4212224cf7ce9b64548c5158f3d8bb
liljdot <jehuoki@gmail.com>
2025-03-09 21:48:04 +0100

    remove axios namespaces and references

--- Client/src/api/productsApi.ts
+++ Client/src/api/productsApi.ts
@@ -2,24 +2,24 @@ import { baseUrl } from ".";
 import axios from "axios";
 import { CreateProductResponseType, GetProductsResponseType, GetSingleProductResponseType } from "../types/apiTypes";
 import { NewProduct, Product } from "../types";
-import { Axios } from "axios/";
 
-export const fetchProducts: () => Axios.IPromise<Axios.AxiosXHR<GetProductsResponseType>> = () => {
+
+export const fetchProducts = () => {
     return axios.get<GetProductsResponseType>(`${baseUrl}/api/products`)
 }
 
-export const fetchSingleProduct: (id: number) => Axios.IPromise<Axios.AxiosXHR<GetSingleProductResponseType>> = (id) => {
+export const fetchSingleProduct = (id: number) => {
     return axios.get<GetSingleProductResponseType>(`${baseUrl}/api/products/${id}`)
 }
 
-export const addProduct: (newProduct: NewProduct) => Axios.IPromise<Axios.AxiosXHR<CreateProductResponseType>> = ({ name, price, image }) => {
+export const addProduct = ({ name, price, image }: NewProduct) => {
     return axios.post<CreateProductResponseType>(`${baseUrl}/api/products`, { name, price, image })
 }
 
-export const updateProduct: (product: Omit<Product, "updated_at" | "created_at">) => Axios.IPromise<Axios.AxiosXHR<GetSingleProductResponseType>> = ({ id, name, price, image }) => {
+export const updateProduct = ({ id, name, price, image }: Omit<Product, "updated_at" | "created_at">) => {
     return axios.patch<GetSingleProductResponseType>(`${baseUrl}/api/products/${id}`, { name, price, image })
 }
 
-export const deleteProduct: (id: number) => Axios.IPromise<Axios.AxiosXHR<void>> = (id) => {
+export const deleteProduct = (id: number) => {
     return axios.delete<void>(`${baseUrl}/api/products/${id}`)
 }
